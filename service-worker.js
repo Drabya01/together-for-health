37d56ffaae58006bf9aaec021db0a0d5cfa1f6e1
Drabya01 <shishir.saud@kensingtontours.com>
2026-06-24 20:39:29 -0400

    Resources: clean two-column reference (Option 2) with short plain blurbs, no icons/emoji/stars; bump SW to v12

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'tfh-v11';
+const CACHE = 'tfh-v12';
 const ASSETS = ['./index.html', './admin-editor.js', './admin-editor.css', './manifest.json', './icon.svg', './icon-maskable.svg'];
 
 self.addEventListener('install', e => {
